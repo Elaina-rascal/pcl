c2986454a6dd30263aa39e6acd149849c0ea8f39
Elaina <1463967532@qq.com>
2024-10-20 22:24:26 +0800

    自动提交: 2024-10-20 22:24:26
    更改文件:
    A	.clang-format
    M	CMakeLists.txt
    M	loadtest.cpp

--- loadtest.cpp
+++ loadtest.cpp
@@ -9,25 +9,33 @@
  *
  */
 
+#include "fmt/core.h"
 #include "pcl/io/pcd_io.h"
 #include <filesystem>
-#include <memory>
+#include <pcl/common/common.h>
 #include <pcl/impl/point_types.hpp>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-int main(int argc, char **argv) {
-  using namespace std;
-  unique_ptr<pcl::PointCloud<pcl::PointXYZ>> cloud_ptr;
-  cloud_ptr.reset(new pcl::PointCloud<pcl::PointXYZ>);
-  if (pcl::io::loadPCDFile(
-          static_cast<string>(filesystem::path(__FILE__).parent_path()) +
-              "/data/tutorials/ism_test_cat.pcd",
-          *cloud_ptr) == -1) {
-    PCL_ERROR("Couldn't read file test_pcd.pcd \n");
-    return -1;
-  } else {
-    cout << "Loaded " << cloud_ptr->width * cloud_ptr->height
-         << " data points from test_pcd.pcd with the following fields: "
-         << endl;
-  }
+int main(int argc, char **argv)
+{
+    using namespace std;
+    unique_ptr<pcl::PointCloud<pcl::PointXYZ>> cloud_ptr;
+    cloud_ptr.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    // Fill in the cloud data
+    if (pcl::io::loadPCDFile(
+            static_cast<string>(filesystem::path(__FILE__).parent_path()) +
+                "/data/tutorials/ism_test_cat.pcd",
+            *cloud_ptr) == -1)
+    {
+        PCL_ERROR("Couldn't read file test_pcd.pcd \n");
+        return -1;
+    }
+    else
+    {
+        fmt::print(
+            "Loaded {} data points from test_pcd.pcd with the following fields: \n",
+            cloud_ptr->width * cloud_ptr->height);
+        pcl::PointXYZ min_pt, max_pt;
+        pcl::getMinMax3D(*cloud_ptr, min_pt, max_pt);
+    }
 }
